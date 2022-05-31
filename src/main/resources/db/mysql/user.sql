bee54e3a5955b440bf0a06ecd3fba6446acb05c0
Kostiantyn Ahafonov <konstantin.in.ua@gmail.com>
2022-05-31 12:10:18 +0300

    color1

--- src/main/resources/db/mysql/user.sql
+++ src/main/resources/db/mysql/user.sql
@@ -1,7 +1,8 @@
+CREATE USER 'petclinic'@'%' IDENTIFIED BY 'petclinic';
 CREATE DATABASE IF NOT EXISTS petclinic;
 
 ALTER DATABASE petclinic
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
-GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic'@'%' IDENTIFIED BY 'petclinic';
+GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic'@'%';
